e3b23179341199f20494c111918641200c64f105
LaoluNET <ayinde.olaoluwa@yahoo.com>
2022-09-12 15:33:46 -0700

    3.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,14 +16,12 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-
 		putchar(low);
 
 	for (low 'A'; low <= 'Z'; low++)
-
 		putchar(low);
 
-		putchat('\n');
+	putchat('\n');
 
 	return (0);
 }
