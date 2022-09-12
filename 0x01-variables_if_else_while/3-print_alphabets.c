7ec88cdf1f1f2ae055d4e248bd2a852a15363d78
LaoluNET <ayinde.olaoluwa@yahoo.com>
2022-09-12 15:27:55 -0700

    3.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/*
+ * main entry point
+ *
+ * return: always 0
+ *
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+
+		putchar(low);
+
+	for (low 'A'; low <= 'Z'; low++)
+
+		putchar(low);
+
+		putchat('\n');
+
+	return (0);
+}
