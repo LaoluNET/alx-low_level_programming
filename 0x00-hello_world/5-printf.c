6a80a9e4f03cd7cd5ea52d96b2d896c9118a3cfa
LaoluNET <ayinde.olaoluwa@yahoo.com>
2022-09-12 16:27:22 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
 * main - entry point
-* return 0
+*
+* Return: Always 0 (success)
+*
 */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piesce of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
