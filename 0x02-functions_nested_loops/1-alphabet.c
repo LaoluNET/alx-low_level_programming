1d07afc6e0ecc71b5f98512744c2bf4dcb0406f9
LaoluNET <ayinde.olaoluwa@yahoo.com>
2022-09-13 13:04:30 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * print-alphabet - a function that prints the alphabeths in lower case
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet(void);
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	
+	{
+
+		_putchar(n);
+
+	}
+
+	_putchar('\n');
+
+}
