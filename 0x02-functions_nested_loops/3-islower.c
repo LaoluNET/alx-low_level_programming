6ab2f000ad906c1533f552afbc927679e98abd90
LaoluNET <ayinde.olaoluwa@yahoo.com>
2022-09-13 14:32:12 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * _islower - tests whether a character is a
- * Return: 1 if it is a lowercase letter
+ * Return: 0 if it is a lowercase letter
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
 		return (0);
 
